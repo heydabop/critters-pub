856cf998b5d546c2c7fce98bf8f6feb5184dfc1c
Ross Dixon <heydabop@gmail.com>
2013-10-12 05:51:47 -0500

    auths. doesnt do anything with it

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,13 @@
 var oauth = ChromeExOAuth.initBackgroundPage({
-  'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
-  'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
-  'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
-  'consumer_key': 'anonymous',
-  'consumer_secret': 'anonymous',
-  'app_name': 'Critters'
+    'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+    'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+    'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+    'consumer_key': 'anonymous',
+    'consumer_secret': 'anonymous',
+    'scope': 'https://www.googleapis.com/auth/userinfo.email',
+    'app_name': 'Critters'
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: "http://google.com/"});
-    //oauth.authorize(function(){});
+    oauth.authorize(function(){});
 });
