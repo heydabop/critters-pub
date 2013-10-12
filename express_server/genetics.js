ec7c82bbae142b186dcf6a1c165fa27552496bd0
Robert Schumacher <robertallenschumacher@gmail.com>
2013-10-12 13:20:39 -0500

    Added genetics.js stub

---
+++ express_server/genetics.js
@@ -0,0 +1,30 @@
+function select_random(myArray) {
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+var specs = ["ball", "ball2", "disk", "duck", "duck2", "plant-nin2", "plant1", "plant2"];
+
+
+function new_genome(species) {
+    return species;
+}
+
+function get_species(genome) {
+    return genome;
+}
+
+function cross_genomes(gen1, gen2) {
+    return select_random(specs);
+}
+
+function get_starter_species() {
+    return ["plant1", "ball"];
+}
+
+function get_hue(genome) { return 0; }
+
+exports = { new_genome: new_genome,
+            get_species: get_species,
+            get_hue: get_hue,
+            cross_genomes: cross_genomes,
+            get_starter_species: get_starter_species }
