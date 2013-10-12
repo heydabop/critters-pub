56b0398351e79e0b0732bed2b98992bbd14f8718
Ross Dixon <heydabop@gmail.com>
2013-10-12 03:52:10 -0500

    best extension. press the button. do it

--- extension/changePage.js
+++ extension/changePage.js
@@ -1,3 +1,13 @@
-function changePage(){
-    chrome.tabs.create({url: "http://google.com"});
+document.querySelector("#hello").onclick = function(){
+    //chrome.tabs.create({url: "http://google.com"});
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var current = tabs[0];
+    alert(current);
+    alert(current);
+    alert(current);
+    alert(current);
+    alert(current);
+    alert(current);
+    alert(current);
+    });
 }
