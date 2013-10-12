7816836feac7346d26f6e533206dcfa7ab7cdf8b
Robert Schumacher <robertallenschumacher@gmail.com>
2013-10-12 07:17:56 -0500

    Improved payload

--- payload.js
+++ payload.js
@@ -1,3 +1,60 @@
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/shuffle [v1.0]
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+function get_elems() {
+    var elems = [];
+
+    ['p', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'span'].forEach(function(tag) {
+        var ps = $(tag);
+        ps.each(function(i, v) {
+            var st = $(v).text();
+            if (st.match(/^\s*$/))
+                return;
+            if (v.offsetWidth !== 0 || v.offsetHeight !== 0)
+                elems.push(v);
+        });
+    });
+
+    var imgs = $('img');
+    imgs.each(function(i, v) {
+        if (v.offsetWidth !== 0 || v.offsetHeight !== 0)
+            elems.push(v);
+    });
+
+    return elems;
+}
+
+function analyze_page(elems) {
+    function pic_points(p, elem) {
+        if (elem.tagName.toLowerCase() == "img") {
+            return elem.offsetHeight * elem.offsetWidth / 1024 + p;
+        }
+        return p;
+    }
+    function text_points(p, elem) {
+        var len = $(elem).text().length;
+        // st = st.replace(/[ ]{2,}/gi," ");
+        // st = st.replace(/\n /,"\n");
+        // var l = st.split(' ').length;
+        return p + len / 10;
+    }
+
+    var points = {
+        "pics": elems.reduce(pic_points, 0),
+        "text": elems.reduce(text_points, 0)
+    };
+    // var acc = 0;
+    // for (k in points) acc += points[k];
+    // for (k in points) {
+    //     points[k] /= 1.0*acc;
+    // }
+    return points;
+}
+
 function go_do() {
     console.log($.fn.jquery);
 
@@ -26,43 +83,7 @@ function go_do() {
         'z-index:99999;'+
         '">';
 
-    //+ Jonas Raoni Soares Silva
-    //@ http://jsfromhell.com/array/shuffle [v1.0]
-    function shuffle(o){ //v1.0
-        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        return o;
-    };
-    var elems = [];
-
-    var ps = $('p');
-    ps.each(function(i, v) {
-        var st = $(v).text();
-        if (st.match(/^\s*$/))
-            return;
-        st = st.replace(/[ ]{2,}/gi," ");
-        st = st.replace(/\n /,"\n");
-        var l = st.split(' ').length;
-        if (v.offsetWidth !== 0 || v.offsetHeight !== 0)
-            elems.push(v);
-    });
-
-    var as = $('a');
-    as.each(function(i, v) {
-        var st = $(v).text();
-        if (st.match(/^\s*$/))
-            return;
-        st = st.replace(/[ ]{2,}/gi," ");
-        st = st.replace(/\n /,"\n");
-        var l = st.split(' ').length;
-        if (v.offsetWidth !== 0 || v.offsetHeight !== 0)
-            elems.push(v);
-    });
-
-    var imgs = $('img');
-    imgs.each(function(i, v) {
-        if (v.offsetWidth !== 0 || v.offsetHeight !== 0)
-            elems.push(v);
-    });
+    var elems = get_elems();
 
     elems.sort(function(a,b){
         return findPos(a)[1] - findPos(b)[1];
@@ -78,16 +99,26 @@ function go_do() {
             return foo();
         }
 
-        console.log("moving to nom");
+        var dist = Math.pow(pos[0] - $('#critter')[0].offsetLeft, 2)
+            + Math.pow(pos[1] - $('#critter')[0].offsetTop, 2);
+        var t = Math.max(300, Math.sqrt(dist));
+
         $('#critter').animate({
             left: pos[0],
             top: pos[1]
-        }, 2000, function() {
-            $(elems[0]).animate({opacity: 0.0}, 2000);
+        }, t, function() {
+            var wdist = elems[0].offsetWidth-50;
+            var hdist = elems[0].offsetHeight-50;
+            if (wdist < 0) wdist = 0;
+            if (hdist < 0) hdist = 0;
+            var dist = wdist*wdist + hdist*hdist;
+            var t = Math.max(500, Math.sqrt(dist)*2);
+
+            $(elems[0]).animate({opacity: 0.0}, t);
             $('#critter').animate({
-                left: "+="+(elems[0].offsetWidth-50),
-                top: "+="+(elems[0].offsetHeight-50)
-            }, 2000, function() {
+                left: "+="+wdist,
+                top: "+="+hdist
+            }, t, function() {
                 elems[0].remove();
                 elems.shift();
                 foo();
@@ -97,4 +128,4 @@ function go_do() {
     foo();
 }
 
-go_do();
+alert(JSON.stringify(analyze_page(get_elems())));
