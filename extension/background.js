26893be5e2f89c8fac6a4e2d9da813db30e9dad4
Ross Dixon <heydabop@gmail.com>
2013-10-12 05:41:09 -0500

    things and what not, still working on oauth

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,13 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-    var newUrl = "inventory.html";
-    chrome.tabs.create({url: newUrl});
+var oauth = ChromeExOAuth.initBackgroundPage({
+  'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+  'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+  'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+  'consumer_key': 'anonymous',
+  'consumer_secret': 'anonymous',
+  'app_name': 'Critters'
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({url: "http://google.com/"});
+    //oauth.authorize(function(){});
 });
--- extension/changePage.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#hello").onclick = function(){
-    //chrome.tabs.create({url: "http://google.com"});
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var current = tabs[0];
-        chrome.tabs.executeScript(current.id, {file: "inject.js"}, function(){});
-        chrome.tabs.executeScript(current.id, {code: "customAlert('" + current.url + "')"}, function(){});
-        //alert(current.url);
-    });
-}
