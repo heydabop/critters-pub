37949f0eecee3c7734d0677e7425b53af05e5c4c
Ross Dixon <heydabop@gmail.com>
2013-10-12 04:01:54 -0500

    now alert is in center

--- extension/changePage.js
+++ extension/changePage.js
@@ -1,7 +1,8 @@
 document.querySelector("#hello").onclick = function(){
     //chrome.tabs.create({url: "http://google.com"});
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var current = tabs[0];
-    alert(current);
+        var current = tabs[0];
+        chrome.tabs.executeScript(current.id, {file: "inject.js"}, function(){});
+        //alert(current.url);
     });
 }
