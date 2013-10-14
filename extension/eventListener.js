e0287ddf2e31fb6a6d317df473ad07e4dc30d0b9
heydabop <heydabop@gmail.com>
2013-10-13 20:09:47 -0500

    oh god whitespace

--- extension/eventListener.js
+++ extension/eventListener.js
@@ -4,9 +4,9 @@ function receiveMessage(event){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var current = tabs[0];
         chrome.tabs.executeScript(current.id, {file: "jquery.min.js"}, function(){
-	    chrome.tabs.executeScript(current.id, {file: "nomnom.js"}, function(){
-		chrome.tabs.executeScript(current.id, {code: "go_do('" + event.data + "')"}, function(){});
-	    });
-	});        
+            chrome.tabs.executeScript(current.id, {file: "nomnom.js"}, function(){
+                chrome.tabs.executeScript(current.id, {code: "go_do('" + event.data + "')"}, function(){});
+            });
+        });
     });
 }
